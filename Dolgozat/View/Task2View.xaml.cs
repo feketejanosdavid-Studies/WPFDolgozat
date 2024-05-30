8a10b847e713b9022cfc6d89dbda41486baa369b
feketejanosdavid <janosdavidfekete@gmail.com>
2024-05-30 10:40:05 +0200

    TASK1-et nem nyitja meg

---
+++ Dolgozat/View/Task2View.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dolgozat.View
+{
+    /// <summary>
+    /// Interaction logic for Task2View.xaml
+    /// </summary>
+    public partial class Task2View : UserControl
+    {
+        public Task2View()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Dolgozat/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Dolgozat.Command;
+using Dolgozat.View;
+
+namespace Dolgozat.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set { currentView = value; OnPropertyChanged(); }
+        }
+
+        Task1View task1View;
+        Task2View task2View;
+        Task3View task3view;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RelayCommand openTask1 { get; }
+        public RelayCommand openTask2 { get; }
+        public RelayCommand openTask3 { get; }
+
+        private void OnPropertyChanged([CallerMemberName]string name=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public MainViewModel()
+        {
+            task1View = new Task1View();
+            task2View = new Task2View();
+            task3view = new Task3View();
+
+            openTask1 = new RelayCommand(x => CurrentView = task1View);
+            openTask2 = new RelayCommand(x => CurrentView = task2View);
+            openTask3 = new RelayCommand(x => CurrentView = task3view);
+
+            currentView = task1View;
+        }
+    }
+}
